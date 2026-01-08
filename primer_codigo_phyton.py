2bbc1e2cf83fd77973dc7be6f2608b11d2927474
joseoropezam-a11y <jose.oropeza.m@ucb.edu.bo>
2026-01-08 12:30:04 -0400

    Add files via upload

---
+++ primer_codigo_phyton.py
@@ -0,0 +1,17 @@
+print("Hello, World!")
+
+x = 60
+y = 7
+
+resultado = x + y
+
+print("Hola, Alfredo es: ")
+print(resultado)
+
+print("-------------------------------")
+
+print("calcular el area de un rectangulo")
+base = 10
+altura = 57
+area_rectangulo = base * altura
+print(area_rectangulo)
